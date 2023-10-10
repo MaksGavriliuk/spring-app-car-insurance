1d75759feae2ff52ee9616ce29734b6cb9a4d89e
MaksGavriliuk <maks.revival27@gmail.com>
2023-10-11 00:16:20 +0300

    create project car-insurance

---
+++ src/main/java/com/example/carinsurance/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.carinsurance;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CarInsuranceApplication.class);
+    }
+
+}
