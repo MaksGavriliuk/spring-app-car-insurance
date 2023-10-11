fe72892a844ed6280c5729e233756fd198b6d8f7
MaksGavriliuk <maks.revival27@gmail.com>
2023-10-11 13:14:33 +0300

    create interface EngineVolumeRepository

---
+++ src/main/java/com/example/carinsurance/repositories/EngineVolumeRepository.java
@@ -0,0 +1,7 @@
+package com.example.carinsurance.repositories;
+
+import com.example.carinsurance.models.EngineVolume;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EngineVolumeRepository extends JpaRepository<EngineVolume, Integer> {
+}
