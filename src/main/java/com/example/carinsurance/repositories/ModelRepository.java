93e0829a387f64fd23402269d9aa68851e48327e
MaksGavriliuk <maks.revival27@gmail.com>
2023-10-11 10:31:35 +0300

    create ModelService

--- src/main/java/com/example/carinsurance/repositories/ModelRepository.java
+++ src/main/java/com/example/carinsurance/repositories/ModelRepository.java
@@ -1,5 +1,6 @@
 package com.example.carinsurance.repositories;
 
+import com.example.carinsurance.models.Brand;
 import com.example.carinsurance.models.Model;
 import org.springframework.data.jpa.repository.JpaRepository;
 //import org.springframework.ui.Model;
@@ -7,5 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ModelRepository extends JpaRepository<Model, Integer> {
-//    List<Model> findByBrandName(String brandName);
+    List<Model> findByModel(String modelName);
+    List<Model> findByBrand(String brandName);
 }
--- src/main/java/com/example/carinsurance/services/ModelService.java
+++ src/main/java/com/example/carinsurance/services/ModelService.java
@@ -1,36 +1,66 @@
-//package com.example.carinsurance.services;
-//
-//import com.example.carinsurance.repositories.BrandRepository;
-//import com.example.carinsurance.repositories.ModelRepository;
-//import com.example.carinsurance.models.Model;
-//import com.example.carinsurance.models.Brand;
-//import lombok.AllArgsConstructor;
-//import lombok.NoArgsConstructor;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.stereotype.Service;
-//
-//@Service
+package com.example.carinsurance.services;
+
+import com.example.carinsurance.repositories.BrandRepository;
+import com.example.carinsurance.repositories.ModelRepository;
+import com.example.carinsurance.models.Model;
+import com.example.carinsurance.models.Brand;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 //@Slf4j
 //@RequiredArgsConstructor
 //@AllArgsConstructor
 //@NoArgsConstructor
-//public class ModelService {
-//
-//    private ModelRepository modelRepository;
-//    private BrandRepository brandRepository;
-//
-//
-//    public Model createModel(String modelName, String brandName) {
-//        Brand brand = (Brand) brandRepository.findByBrand(brandName);
-//        if (brand != null) {
-//            // Обработка отсутствия бренда
-//        }
-//
-//        Model model = new Model();
-//        model.setModel(modelName);
-//        model.setBrand(brand);
-//        return null;
-////        return modelRepository.save(model);
-//    }
-//}
+public class ModelService {
+
+    private ModelRepository modelRepository;
+    private BrandRepository brandRepository;
+
+
+    public List<Model> listModels(String modelName) {
+        if (modelName != null) {
+            return modelRepository.findByModel(modelName);
+        }
+        return modelRepository.findAll();
+    }
+
+    public List<Model> getModelsByBrand(String brandName) {
+        return modelRepository.findByBrand(brandName);
+    }
+
+    public void saveModel(Model model, String brandName) {
+
+        List<Brand> brands = brandRepository.findByBrand(brandName);
+        Brand brand;
+
+        if (brands.isEmpty()) {
+            brand = new Brand();
+            brand.setBrand(brandName);
+            brandRepository.save(brand);
+        } else {
+            brand = brands.get(0);
+        }
+
+        model.setBrand(brand);
+        modelRepository.save(model);
+
+    }
+
+    public void deleteModel(int id) {
+        modelRepository.deleteById(id);
+    }
+
+    public Model getModelById(int id) {
+        return modelRepository.findById(id).orElse(null);
+    }
+
+
+
+}
