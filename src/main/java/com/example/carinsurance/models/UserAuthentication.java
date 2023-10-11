b77cce9aebe20a7d68fd2eae30ffc94e6494fe40
MaksGavriliuk <maks.revival27@gmail.com>
2023-10-11 14:28:16 +0300

    create model UserAuthentication

---
+++ src/main/java/com/example/carinsurance/models/UserAuthentication.java
@@ -0,0 +1,36 @@
+package com.example.carinsurance.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Data
+@Entity
+@Table(name = "user_authentications")
+public class UserAuthentication {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+
+    public void setPassword(String password) {
+        this.password = new BCryptPasswordEncoder().encode(password);
+    }
+
+    public boolean isPasswordValid(String password) {
+        return new BCryptPasswordEncoder().matches(password, this.password);
+    }
+
+}
