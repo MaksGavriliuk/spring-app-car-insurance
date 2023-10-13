ed006a257da991c88a31b97288657fa14efafc24
MaksGavriliuk <maks.revival27@gmail.com>
2023-10-13 20:09:51 +0300

    create AdminService

---
+++ src/main/java/com/example/carinsurance/services/AdminService.java
@@ -0,0 +1,31 @@
+package com.example.carinsurance.services;
+
+import com.example.carinsurance.models.Admin;
+import com.example.carinsurance.models.Model;
+import com.example.carinsurance.repositories.AdminRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AdminService {
+
+    @Autowired
+    private AdminRepository adminRepository;
+
+    public List<Admin> listAdmins() {
+        return adminRepository.findAll();
+    }
+
+
+
+    public void deleteAdmin(int id) {
+        adminRepository.deleteById(id);
+    }
+
+    public Admin getAdminById(int id) {
+        return adminRepository.findById(id).orElse(null);
+    }
+
+}
