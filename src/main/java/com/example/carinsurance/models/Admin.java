f7228bd47d096346f2ee00786b9ce05085605d96
MaksGavriliuk <maks.revival27@gmail.com>
2023-10-13 19:58:39 +0300

    create model Admin

---
+++ src/main/java/com/example/carinsurance/models/Admin.java
@@ -0,0 +1,47 @@
+package com.example.carinsurance.models;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "admins")
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+//    @JsonIgnore
+    @OneToOne
+    @JoinColumn(name = "user_authentication_id")
+    private UserAuthentication userAuthentication;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "patronymic")
+    private String patronymic;
+
+}
--- src/main/java/com/example/carinsurance/models/UserAuthentication.java
+++ src/main/java/com/example/carinsurance/models/UserAuthentication.java
@@ -1,13 +1,19 @@
 package com.example.carinsurance.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.List;
 
 @Data
 @Entity
@@ -23,6 +29,10 @@ public class UserAuthentication {
 
     @Column(name = "password")
     private String password;
+
+    @JsonIgnore
+    @OneToOne(mappedBy = "userAuthentication", cascade = CascadeType.ALL)
+    private Admin admin;
 
 
     public void setPassword(String password) {
