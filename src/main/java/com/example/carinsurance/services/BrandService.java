70a57fca10e78b9ca3e55b9b27aa7746712a8e0d
MaksGavriliuk <maks.revival27@gmail.com>
2023-10-13 19:36:05 +0300

    added getModelsByBrand to BrandService

--- src/main/java/com/example/carinsurance/services/BrandService.java
+++ src/main/java/com/example/carinsurance/services/BrandService.java
@@ -1,11 +1,14 @@
 package com.example.carinsurance.services;
 
 import com.example.carinsurance.models.Brand;
+import com.example.carinsurance.models.Model;
 import com.example.carinsurance.repositories.BrandRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -32,6 +35,11 @@ public class BrandService {
 
     public Brand getBrandById(int id) {
         return brandRepository.findById(id).orElse(null);
+    }
+
+    public List<Model> getModelsByBrand(String brandName) {
+        List<Brand> brands = brandRepository.findByBrand(brandName);
+        return (brands.isEmpty()) ? Collections.emptyList() : brands.get(0).getModels();
     }
 
 }
