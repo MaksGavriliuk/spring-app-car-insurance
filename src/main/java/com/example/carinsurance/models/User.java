164b4cc8996d07f7df388f415f5e4c7d8d5f1665
MaksGavriliuk <maks.revival27@gmail.com>
2023-10-14 11:16:04 +0300

    create Model User

---
+++ src/main/java/com/example/carinsurance/models/User.java
@@ -0,0 +1,49 @@
+package com.example.carinsurance.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+
+@Data
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @OneToOne
+    @JoinColumn(name = "user_authentication_id")
+    private UserAuthentication userAuthentication;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "patronymic")
+    private String patronymic;
+
+    @Column(name = "sex")
+    private String sex;
+
+    @Column(name = "age")
+    private int age;
+
+    @Column(name = "experience")
+    private int experience;
+
+}
