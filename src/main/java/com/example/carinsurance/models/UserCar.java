c64d79cbdb7d18af83bd5d343c60fcba8a480349
MaksGavriliuk <maks.revival27@gmail.com>
2023-10-18 00:16:55 +0300

    create model UserCar

---
+++ src/main/java/com/example/carinsurance/models/UserCar.java
@@ -0,0 +1,35 @@
+package com.example.carinsurance.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name = "user_cars")
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserCar {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "car_id")
+    private Car car;
+
+}
