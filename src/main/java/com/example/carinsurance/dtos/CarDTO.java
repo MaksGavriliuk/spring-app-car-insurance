ac50cafe44960e1193ba39b8270292b5ec08b4e6
MaksGavriliuk <maks.revival27@gmail.com>
2023-10-17 13:21:20 +0300

    create CarDTO

---
+++ src/main/java/com/example/carinsurance/dtos/CarDTO.java
@@ -0,0 +1,17 @@
+package com.example.carinsurance.dtos;
+
+import lombok.Data;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+
+
+@Data
+public class CarDTO {
+    private int id;
+    private int modelId;
+    private int engineVolumeId;
+    private int fuelTypeId;
+    private int productionYear;
+    private BigDecimal currentValue;
+}
