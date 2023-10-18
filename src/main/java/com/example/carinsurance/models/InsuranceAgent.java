ad44796c25c51e6731a691a34246dbc35a28d341
MaksGavriliuk <maks.revival27@gmail.com>
2023-10-18 13:21:28 +0300

    create model InsuranceAgent

---
+++ src/main/java/com/example/carinsurance/models/InsuranceAgent.java
@@ -0,0 +1,52 @@
+package com.example.carinsurance.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.math.BigDecimal;
+
+
+@Data
+@Entity
+@Table(name = "insurance_agents")
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class InsuranceAgent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @OneToOne
+    @JoinColumn(name = "user_authentication_id")
+    private UserAuthentication userAuthentication;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "patronymic")
+    private String patronymic;
+
+    @Column(name = "sex")
+    private String sex;
+
+    @Column(name = "age")
+    private int age;
+
+    @Column(name = "profit", precision = 10, scale = 2)
+    private BigDecimal experience;
+
+}
