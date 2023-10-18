61ed88c1bbe2fa6afec35833b79ce8e3c07c6d08
MaksGavriliuk <maks.revival27@gmail.com>
2023-10-18 11:18:25 +0300

    create UserCarController

--- src/main/java/com/example/carinsurance/controllers/UserCarController.java
+++ src/main/java/com/example/carinsurance/controllers/UserCarController.java
@@ -1,14 +1,21 @@
 package com.example.carinsurance.controllers;
 
-
+import com.example.carinsurance.dtos.UserCarDTO;
 import com.example.carinsurance.models.UserCar;
 import com.example.carinsurance.services.UserCarService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
 
 @RestController
 @RequestMapping("/user-cars")
@@ -21,6 +28,23 @@ public class UserCarController {
     @GetMapping
     public List<UserCar> getUserCars(){
         return userCarService.listUserCars();
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Void> createUserCar(@RequestBody UserCarDTO userCarDTO) {
+        userCarService.saveUserCar(userCarDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUserCar(@PathVariable Integer id) {
+        userCarService.deleteUserCar(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    public UserCar getUserCarById(@PathVariable Integer id) {
+        return userCarService.getUserCarById(id);
     }
 
 
