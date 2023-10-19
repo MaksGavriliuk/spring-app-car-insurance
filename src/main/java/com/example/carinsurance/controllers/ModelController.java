a74f0b620ff8ab95aa365f5f23e423a8f4df00d4
MaksGavriliuk <maks.revival27@gmail.com>
2023-10-19 23:17:58 +0300

    add updateModel to ModelController

--- src/main/java/com/example/carinsurance/controllers/ModelController.java
+++ src/main/java/com/example/carinsurance/controllers/ModelController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -41,6 +42,12 @@ public class ModelController {
     public ResponseEntity<Void> deleteModel(@PathVariable Integer id) {
         modelService.deleteModel(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateModel(@PathVariable Integer id, @RequestBody ModelDTO modelDTO) {
+        modelService.updateModel(id, modelDTO);
+        return ResponseEntity.ok().build();
     }
 
     @GetMapping("/{id}")
