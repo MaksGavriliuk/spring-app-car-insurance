f01c0161bbec61081466a240b6ae97544b9f5aa7
MaksGavriliuk <maks.revival27@gmail.com>
2023-11-15 00:40:46 +0300

    create AuthenticationResponse

--- src/main/java/com/example/carinsurance/auth/AuthenticationController.java
+++ src/main/java/com/example/carinsurance/auth/AuthenticationController.java
@@ -1,6 +1,7 @@
 package com.example.carinsurance.auth;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
