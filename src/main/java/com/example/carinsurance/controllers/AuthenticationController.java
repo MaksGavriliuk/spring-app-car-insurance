6cbf6b2a5f86bbfed66b3830266db20d70b4c5c1
MaksGavriliuk <maks.revival27@gmail.com>
2023-11-15 00:37:26 +0300

    create AuthenticationController

---
+++ src/main/java/com/example/carinsurance/controllers/AuthenticationController.java
@@ -0,0 +1,26 @@
+package com.example.carinsurance.controllers;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
+        return null;
+    }
+
+    @PostMapping("/authentication")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody AuthenticationRequest request) {
+        return null;
+    }
+
+}
