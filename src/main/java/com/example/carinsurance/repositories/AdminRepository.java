ddab7ba891c41b14183f3696c33071b97e9bd886
MaksGavriliuk <maks.revival27@gmail.com>
2023-11-16 12:43:14 +0300

    update AdminRepository: add findByUserAuthentication

--- src/main/java/com/example/carinsurance/repositories/AdminRepository.java
+++ src/main/java/com/example/carinsurance/repositories/AdminRepository.java
@@ -1,7 +1,11 @@
 package com.example.carinsurance.repositories;
 
 import com.example.carinsurance.models.Admin;
+import com.example.carinsurance.models.UserAuthentication;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AdminRepository extends JpaRepository<Admin, Integer> {
+    Optional<Admin> findByUserAuthentication(UserAuthentication userAuthentication);
 }
