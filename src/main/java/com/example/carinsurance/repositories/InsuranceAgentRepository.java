5e1cc17c24b937ac8d9ce3995fde054a9b6cae1e
MaksGavriliuk <maks.revival27@gmail.com>
2023-11-16 12:43:45 +0300

    update IsuranceAgentRepository: add findByUserAuthentication

--- src/main/java/com/example/carinsurance/repositories/InsuranceAgentRepository.java
+++ src/main/java/com/example/carinsurance/repositories/InsuranceAgentRepository.java
@@ -1,7 +1,12 @@
 package com.example.carinsurance.repositories;
 
+import com.example.carinsurance.models.Admin;
 import com.example.carinsurance.models.InsuranceAgent;
+import com.example.carinsurance.models.UserAuthentication;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface InsuranceAgentRepository extends JpaRepository<InsuranceAgent, Integer> {
+    Optional<InsuranceAgent> findByUserAuthentication(UserAuthentication userAuthentication);
 }
