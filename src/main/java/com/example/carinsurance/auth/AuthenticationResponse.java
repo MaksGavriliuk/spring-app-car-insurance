c7008a2de446f154f8bf5cbce5394036fc434c9a
MaksGavriliuk <maks.revival27@gmail.com>
2023-11-17 16:51:43 +0300

    update AuthenticationResponse: add Map<> user

--- src/main/java/com/example/carinsurance/auth/AuthenticationResponse.java
+++ src/main/java/com/example/carinsurance/auth/AuthenticationResponse.java
@@ -6,11 +6,13 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Map;
+
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class AuthenticationResponse {
     private String token;
-    private String role;
+    private Map<String, Object> user;
 }
