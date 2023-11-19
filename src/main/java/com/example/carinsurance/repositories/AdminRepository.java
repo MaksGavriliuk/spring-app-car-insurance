453b23ef5a9a0df65e079b3fb6f61f0565fcac26
MaksGavriliuk <maks.revival27@gmail.com>
2023-11-20 00:00:17 +0300

    update AdminRepository: implements SearchEngineUserByUserAuthentication<Admin>

--- src/main/java/com/example/carinsurance/repositories/AdminRepository.java
+++ src/main/java/com/example/carinsurance/repositories/AdminRepository.java
@@ -1,11 +1,18 @@
 package com.example.carinsurance.repositories;
 
+import com.example.carinsurance.auth.SearchEngineUserByUserAuthentication;
 import com.example.carinsurance.models.Admin;
 import com.example.carinsurance.models.UserAuthentication;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface AdminRepository extends JpaRepository<Admin, Integer> {
+public interface AdminRepository extends JpaRepository<Admin, Integer>, SearchEngineUserByUserAuthentication<Admin> {
     Optional<Admin> findByUserAuthentication(UserAuthentication userAuthentication);
+
+    @Override
+    default Optional<Admin> findUserByUserAuthentication(UserAuthentication userAuthentication){
+        return findByUserAuthentication(userAuthentication);
+    }
+
 }
