656235ac9d147457fa1fae8c971b7e678c51421c
MaksGavriliuk <maks.revival27@gmail.com>
2023-11-19 23:59:30 +0300

    update UserRepository: implements SearchEngineUserByUserAuthentication<User>

--- src/main/java/com/example/carinsurance/repositories/UserRepository.java
+++ src/main/java/com/example/carinsurance/repositories/UserRepository.java
@@ -1,11 +1,20 @@
 package com.example.carinsurance.repositories;
 
+import com.example.carinsurance.auth.SearchEngineUserByUserAuthentication;
 import com.example.carinsurance.models.User;
 import com.example.carinsurance.models.UserAuthentication;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer>, SearchEngineUserByUserAuthentication<User> {
+
     Optional<User> findByUserAuthentication(UserAuthentication userAuthentication);
+
+
+    @Override
+    default Optional<User> findUserByUserAuthentication(UserAuthentication userAuthentication){
+        return findByUserAuthentication(userAuthentication);
+    }
+
 }
