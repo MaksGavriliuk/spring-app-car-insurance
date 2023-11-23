cc0fe61370a0d46427dc7de12d6a373353115071
MaksGavriliuk <maks.revival27@gmail.com>
2023-11-23 17:39:28 +0300

    update UserCarRepository

--- src/main/java/com/example/carinsurance/repositories/UserCarRepository.java
+++ src/main/java/com/example/carinsurance/repositories/UserCarRepository.java
@@ -3,5 +3,8 @@ package com.example.carinsurance.repositories;
 import com.example.carinsurance.models.UserCar;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserCarRepository extends JpaRepository<UserCar, Integer> {
+    Optional<UserCar> findCarsByUserId(int userId);
 }
