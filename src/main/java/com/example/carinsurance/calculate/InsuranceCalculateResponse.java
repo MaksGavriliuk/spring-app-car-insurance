8be3ebc24870fdbb07ddfbcf1a2fa9aa9c5fb933
MaksGavriliuk <maks.revival27@gmail.com>
2023-12-07 16:48:31 +0300

    create InsuranceCalculateResponse

---
+++ src/main/java/com/example/carinsurance/calculate/InsuranceCalculateResponse.java
@@ -0,0 +1,10 @@
+package com.example.carinsurance.calculate;
+
+import lombok.Data;
+
+
+@Data
+public class InsuranceCalculateResponse {
+    private Double amount;
+    private Double payoutAmount;
+}
